12dab318c7f37f52321ed43e587b60f10d4549c7
chiknahuikoatl <jalrod@ciencias.unam.mx>
2019-05-29 02:42:35 -0500

    Estructura básica del Cliente.

---
+++ Proyecto_Final/src/Cliente.java
@@ -0,0 +1,10 @@
+public class Cliente{
+    private Producto[] carrito;
+
+    public Cliente(){}
+
+    // Getters y setters
+    public Producto[] getCarrito(){
+        return this.carrito;
+    }
+}
