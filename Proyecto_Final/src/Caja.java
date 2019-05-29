7b457eb2a6aac8d058e74d06d4f34fce1288f783
chiknahuikoatl <jalrod@ciencias.unam.mx>
2019-05-29 02:24:14 -0500

    Estructura básica de la clase Caja

---
+++ Proyecto_Final/src/Caja.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class Caja{
+    private LinkedList<Cliente> fila; // Linked list que será usada cómo cola
+
+    public Caja(){}
+
+    // Getters y setters
+    public LinkedList<Cliente> getFila(){
+        return this.fila();
+    }
+}
