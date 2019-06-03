2fa1efa81ff61fea7b8dd55b2f53d127b21f6e66
chiknahuikoatl <jalrod@ciencias.unam.mx>
2019-06-02 22:45:13 -0500

    Método para añadir un tipo de producto al carrito.

--- Proyecto_Final/src/Cliente.java
+++ Proyecto_Final/src/Cliente.java
@@ -1,7 +1,25 @@
-public class Cliente{
-    private Producto[] carrito;
+import java.util.LinkedList;
 
-    public Cliente(){}
+public class Cliente{
+    private static Supermercado miSuper;
+    private LinkedList<Producto> carrito;
+
+    public Cliente(Supermercado super){
+        this.miSuper = super();
+    }
+
+    public boolean meteAlCarrito(int prod, int cant) throws
+            YaSeAcaboJovenException{
+        int cantProd = miSuper.enExistencia(prod);
+        if(cantProd <= 0){
+            throw new YaSeAcaboJovenException();
+        }else if(cantProd < cant){
+            carrito.append(new Producto(miSuper.retiraAlmacen(prod, cantProd),
+                cantProd));
+        }else{
+            carrito.append(new Producto(miSuper.getAlmacen()[prod], cant));
+        }
+    }
 
     // Getters y setters
     public Producto[] getCarrito(){
