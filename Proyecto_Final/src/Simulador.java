3027cf7a48779158620da333daaa8b12ce9e21df
Martin <cofy43@ciencias.unam.mx>
2019-06-03 21:47:17 -0500

    Sugerecia de clase principal

---
+++ Proyecto_Final/src/Simulador.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Simulador {
+    public void muestraMenu() {
+        System.out.println("Bienvenido, seleccione una opcion por favor");
+        System.out.println("1) Labor administrativa");
+        System.out.println("2) ejecutar simulacion");
+        System.out.println("3) ejecutar simulacion con un numero especifico de cajas");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Simulador s = new Simulador();
+        s.muestraMenu();
+        int opcion;
+        opcion = sc.nextInt();
+        while (opcion <= 0 || opcion > 3) {
+            System.out.println("Opcion incorrecta");
+            s.muestraMenu();
+            opcion = sc.nextInt();
+        }
+
+        switch(opcion) {
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+    }
+}
